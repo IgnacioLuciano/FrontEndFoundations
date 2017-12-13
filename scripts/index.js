006100623ad1b7b3ce23d917ef8a27179452e215
Unknown <iglumu@gmail.com>
2017-12-13 13:48:09 -0800

    End of Ex5

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const hoursInDay = 24;
+let GOTcharacter;
+GOTcharacter =  {Name : "Jaime Lannister",
+                 House : "Lannister", 
+                 Childcount : "3"
+};
+console.log('GOTcharacter');
+console.log(housrsInDay= '24');
