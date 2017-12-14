8db7b67669b8f57856b9735b4e82e3274fb4c341
Unknown <iglumu@gmail.com>
2017-12-14 15:55:19 -0800

    Ex7

--- scripts/index.js
+++ scripts/index.js
@@ -21,10 +21,25 @@ else {
 }
 
 
+const UndefinedCheck = (valueToCheck) => {
+    if (valueToCheck != undefined) { return 'false' }
+    else { return 'true' }
+}
+
+function addStrings(string1, string2){
+    return string1 + string2;
+}
+
+var fullString = addStrings("I am ", "A full string!"){
+  return : addStrings,
+}
 
 
 
 
-
-
-
+// Using the arrow syntax, create a function called UndefinedCheck that does the following:
+// Takes one parameter called valueToCheck
+// If the valueToCheck parameter is undefined return false otherwise return true
+// Use the not operator (!) in your if expression
+// Refactor the if statement from Exercise 6 to use the UndefinedCheck function and the turnary operator to determine what
+// to write to the console window
